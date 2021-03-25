115d923d1f126e679f9a1d9000dba70231b0b606
Phi-Long Bui <50190432+phi63897@users.noreply.github.com>
2021-03-24 19:15:52 -0700

    Update fin_report_data.py

--- stock_dash/fin_report_data.py
+++ stock_dash/fin_report_data.py
@@ -2,28 +2,36 @@ import pandas as pd
 import requests
 from datetime import date
 import os
+
 def get_financial_report(ticker):
     iex_token = os.getenv('iex_token')
     base_url = "https://cloud.iexapis.com/"
 
     balanceRequest = "stable/stock/{ticker}/balance-sheet?period=annual&last=5&token={token}".format(ticker=ticker, token = iex_token)
-    balanceSheet = request.get(base_url + balanceRequest)["balancesheet"]
+    balanceSheet = requests.get(base_url + balanceRequest).json()["balancesheet"]
 
     cashRequest = "stable/stock{ticker}/cash-flow?period=annual&last=5&token={token}".format(ticker=ticker, token = iex_token)
-    cashFlow = request.get(base_url + cashRequest)["cashflow"]
+    # cashFlowRes = (requests.get(base_url + cashRequest)).json()["cashflow"]
 
     incomeRequest = "stable/stock/{ticker}/income?period=annual&last=5&token={token}".format(ticker=ticker, token = iex_token)
-    incomeStatement = request.get(base_url+incomeRequest)["income"]
+    incomeStatement = requests.get(base_url+incomeRequest).json()["income"]
 
-    eps=["-" * 5]
-    epsGrowth = ["-" * 5]
-    totalasset = reverse([year["totalAssets"] for year in balanceSheet])
-    netIncome = reverse([year["netIncome"] for year in incomeStatement])
-    longtermDebt = reverse([year["longTermDebt"] for year in balanceSheet])
-    interestIncome = reverse([year["interestIncome"] for year in incomeStatement])
-    ebit= reverse([year["ebit"] for year in incomeStatement])
-    shareholderEquity = reverse([year["shareholderEquity"] for year in balanceSheet])
+    eps=["-"]
+    epsGrowth = ["-"]
+    totalasset = [year["totalAssets"] for year in balanceSheet]
+    totalasset.reverse()
+    netIncome = [year["netIncome"] for year in incomeStatement]
+    netIncome.reverse()
+    longtermDebt = [year["longTermDebt"] for year in balanceSheet]
+    longtermDebt.reverse()
+    interestIncome = [year["interestIncome"] for year in incomeStatement]
+    interestIncome.reverse()
+    ebit= [year["ebit"] for year in incomeStatement]
+    ebit.reverse()
+    shareholderEquity = [year["shareholderEquity"] for year in balanceSheet]
+    shareholderEquity.reverse()
     roa = [netIncome[i]/totalasset[i] for i in range(len(totalasset))]
+
 
 
     # load all the data into dataframe
@@ -33,5 +41,3 @@ def get_financial_report(ticker):
     fin_df.reset_index(inplace=True)
 
     return fin_df
-
-
