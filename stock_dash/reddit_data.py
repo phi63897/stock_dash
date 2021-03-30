be4021b496c19ceae2c0a817eee8279d1761bb16
Phi-Long Bui <50190432+phi63897@users.noreply.github.com>
2021-03-30 13:55:43 -0700

    Update reddit_data.py

--- stock_dash/reddit_data.py
+++ stock_dash/reddit_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import praw
 import os
+import sqlite3
 
 r_cid = os.getenv('reddit_api')
 r_csec = os.getenv('reddit_api_sec')
@@ -8,11 +9,14 @@ r_uag = os.getenv('reddit_user')
 
 
 def get_reddit(cid= r_cid, csec= r_csec, uag= r_uag, subreddit='wallstreetbets'):
-   
+    #connect to sqlite database
+    conn = sqlite3.connect(reddit.sqlite')
+                           
     #connect to reddit
     reddit = praw.Reddit(client_id= cid, client_secret= csec, user_agent= uag)
     #get the new reddit posts
-    posts = reddit.subreddit(subreddit).hot(limit=100)
+    posts = reddit.subreddit(subreddit).top('daily', limit=100)
+                           
     #load the posts into a pandas dataframe
     p = []
     for post in posts:
@@ -20,6 +24,11 @@ def get_reddit(cid= r_cid, csec= r_csec, uag= r_uag, subreddit='wallstreetbets')
             p.append([post.title, post.score, post.selftext])
         else:
             p.append([post.title, post.score, post.url])
+                           
+    #create dataframe from list
     posts_df = pd.DataFrame(p,columns=['title', 'score', 'post'])
+                           
+    #save top posts to sqlite database
+    posts_df.to_sql('reddit', conn, if_exists = 'replace')
     
-    return posts_df
+    return print('done')
