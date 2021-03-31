6373a6947e18b0378fd7471927aee4ca37cc5b12
Phi-Long Bui <50190432+phi63897@users.noreply.github.com>
2021-03-31 13:33:21 -0700

    Update main.py

--- stock_dash/main.py
+++ stock_dash/main.py
@@ -44,7 +44,8 @@ layout1 = html.Div([
 dbc.Row([dbc.Col(make_card("Enter Ticker", "success", ticker_inputs('ticker-input', 'date-picker', 36)))]) #row 1
 ,dbc.Row([dbc.Col([make_card("Twitter Order Flow", 'primary', make_table('table-sorting-filtering2', flow, '17px', 10))])
          ,dbc.Col([make_card("Wallstreet Bets Hot Posts", 'primary', make_table('table-sorting-filtering', top_post, '17px', 4))])
-        ])
+        ],
+        no_gutters=True,)#end row
 , dbc.Row(id = 'cards')
 , dbc.Row([
         dbc.Col([make_card("Fin table ", "secondary", html.Div(id="fin-table"))])
