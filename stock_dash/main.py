9cfe6fd350bbd696366aea0c7713dfb5229aaed2
Phi-Long Bui <50190432+phi63897@users.noreply.github.com>
2021-04-01 14:14:34 -0700

    Update main.py

--- stock_dash/main.py
+++ stock_dash/main.py
@@ -266,5 +266,27 @@ def create_graph(ticker,startdate, enddate, n):
         
     return accordion
 
+#callback for displaying stock charts
+@app.callback(
+    [Output(f"collapse-{i}", "is_open") for i in range(1, 4)],
+    [Input(f"group-{i}-toggle", "n_clicks") for i in range(1, 4)],
+    [State(f"collapse-{i}", "is_open") for i in range(1, 4)],
+)
+def toggle_accordion(n1, n2, n3, is_open1, is_open2, is_open3):
+    ctx = dash.callback_context
+
+    if not ctx.triggered:
+        return False, False, False
+    else:
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    if button_id == "group-1-toggle" and n1:
+        return not is_open1, False, False
+    elif button_id == "group-2-toggle" and n2:
+        return False, not is_open2, False
+    elif button_id == "group-3-toggle" and n3:
+        return False, False, not is_open3
+    return False, False, False
+
 if __name__ == '__main__':
     app.run_server(debug=True)
